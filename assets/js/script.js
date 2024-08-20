7374aaccd48cbbea2f4420985ffcf75d2dcfd5df
Augusto Marques de Souza <augustomarquesdesouza@gmail.com>
2024-08-20 20:47:58 -0300

    Ajuste Animação Scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,7 @@ initScroll();
 //Animação ao scroll
 function initAnimaScroll() {
     const sections = document.querySelectorAll('.js-scroll');
-    const windowsMetade = window.innerHeight * 0.68;
+    const windowsMetade = window.innerHeight * 0.8;
     
     function animaScroll() {
         sections.forEach((section) => {
